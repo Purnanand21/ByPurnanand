1f783f17f73cfb7b6d9cd5a21d9da81c596c41af
Purnanand Upadhyay <80934817+Purnanand21@users.noreply.github.com>
2021-05-13 06:38:12 +0100

    Create second.js

---
+++ Data Conversion/second.js
@@ -0,0 +1,40 @@
+//  alert example -->
+// alert('HI there'); // with specified content
+// alert(); // without any specified content
+
+// prompt example
+// let age = prompt('How old are you?');
+
+// alert(`You are ${age} years old!`);
+// let first_name = 'Styam';
+
+// alert("Welcome " + first_name + "!");
+// console.log(first_name);
+
+// let status = confirm("Are you sure?");
+// console.log(status);
+
+// let num1 = "100";
+// let num2 = 200;
+// let num3 = 300
+// let num4 = "sachin";
+
+// document.write(300 + 100 + "200");
+
+// document.write("<br>");
+// document.write("300" - 100);
+
+// console.log(typeof(num1));
+// console.log(typeof(num2));
+// console.log(typeof(num3));
+// console.log(typeof(num4));
+
+// let num1 = prompt("Enter num1: "); // 10
+// let num2 = prompt("Enter num2: "); // 20
+// console.log(typeof(num1));
+// console.log(typeof(num2));
+// let result = num1 + num2;
+// console.log(result);
+
+// STring ---> int , float , number
+// Data type conversion
