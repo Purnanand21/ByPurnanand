67c979496fb4520634707704e77e786b43bfc232
Purnanand Upadhyay <80934817+Purnanand21@users.noreply.github.com>
2021-05-14 17:59:03 +0100

    Create condition.js

---
+++ condition/condition.js
@@ -0,0 +1,26 @@
+// let username = prompt("Enter Your Name");
+// let age = prompt("Enter Your Age");
+
+// if(age>44){
+//     document.write("Welcome" + username + "<br>");
+//     document.write("You Can Take Vacine");
+
+// } else if (age>18){
+//     document.write(username +  "You can Register For Vacine");
+// } else {
+//     document.write("Sorry  "   +  username + "You can not take Vacine");
+// }
+
+// let username = 'purnanand';
+// let password = 'pass123';
+// let user_useername = prompt("Enter Username");
+// let user_password = prompt("Enter Your Password");
+// if (user_useername == user_useername && password == user_password){
+//     document.write("Authentication Successfull");
+// } else{
+//     document.write("Username or Password is Wrong");
+// }
+
+
+
+
