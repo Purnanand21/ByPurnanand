58f0c6d50ae4e4171e01e060af0d87624c16aba1
Purnanand Upadhyay <80934817+Purnanand21@users.noreply.github.com>
2021-05-18 17:07:56 +0100

    Create loop.js

---
+++ Loop/loop.js
@@ -0,0 +1,54 @@
+// Loop 
+
+// For 
+// while --> do while
+
+// while (condition){
+//     statement1;
+//     statement2;
+//     statement3;
+// }
+// let num = 10;
+// //    10 > 20:false
+// while (num > 20) {
+//     document.write(num + "<br>");
+//     num--
+// }
+
+
+// let num = 10;
+
+// do {
+//     document.write(num + "<br>");
+//     num++
+// } while (num < 15);
+
+//      10 < 5 : false
+// authentication ==>
+// intagram --> username , password
+
+// three ->
+// let username = "purnanand";
+// let password = "well2021";
+
+// let attempt = 1;
+
+// while (attempt <= 3) {
+//     let xy = "Enter Username " + attempt + " attempt: ";
+//     let user_username = prompt(xy);
+//     let user_password = prompt("Enter password: ");
+//     if (user_username == username) {
+//         if (user_password == password) {
+//             document.body.innerHTML = '';
+//             document.write("Login Successfull!");
+//             break;
+//         } else {
+//             document.body.innerHTML = '';
+//             document.write("Password is wrong!");
+//         }
+//     } else {
+//         document.body.innerHTML = '';
+//         document.write("Username is Not Defined.");
+//     }
+//     attempt++
+// }
