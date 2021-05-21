18814ea1e3e0dfd6d1a6e7429e7763e340ea3e10
Purnanand Upadhyay <80934817+Purnanand21@users.noreply.github.com>
2021-05-21 12:34:09 +0100

    Create function.js

---
+++ Function/function.js
@@ -0,0 +1,68 @@
+// alert("hi sachin!")
+// alert("hello!");
+
+// function hello()
+// {
+//     document.write("Hello Users! <br>");
+//     document.write("Welcome to My Profile <br>");
+// }
+// hello();
+
+// function add(){
+//     let num1 = 10;
+//     let num2 = 20;
+//     let result = num1+num2;
+//     document.write(result);
+// }
+
+// function add(num1,num2=0){
+//     let result = num1 + num2;
+
+
+//     document.write('Num1: ' + num1 + "<br>");
+//     document.write('Num2: ' + num2 + "<br>");
+//     document.write('Result: ' + result + "<br>");
+//     document.write("--------------<br>")
+
+// }
+// add(5);
+// add(10,15);
+
+// return : --> special keyword -> exit function
+
+// username = 'purnanand';
+// password = 'hello123';
+
+// prompt("Enter username: ")
+
+// function login(user_username, user_password) {
+//     if (user_username == username) {
+//         if (user_password == password) {
+//             return true;
+//         } else {
+//             return false;
+//         }
+//     } else {
+//         return false;
+//     }
+// }
+// console.log(login('purnanand', 'hello123'));
+// if (login('purnanand', 'hello123')) {
+//     document.write("Login Success!");
+// } else {
+//     document.write("Invalid Login!");
+// }
+
+
+// function add(a = 0, b = 0) {
+//     console.log('a', a);
+//     console.log('b', b);
+//     return a + b;
+// }
+
+// let result1 = add(b = 30);
+
+// let result2 = add(100, 30);
+
+// console.log(result1);
+// console.log(result2);
