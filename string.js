727abb12aa3b9f80cb89d74335d638558ff16ea5
Purnanand Upadhyay <80934817+Purnanand21@users.noreply.github.com>
2021-05-22 10:44:20 +0100

    javascript file

---
+++ string.js
@@ -0,0 +1,59 @@
+// String 
+// let num1 = 'purnanand';
+// let result = typeof(num1);
+
+// document.write(result);
+// 
+
+//  ' '   : single quotes
+//  " "   : double quotes
+//  ` `   : backticks ~` , sabstitution
+
+// let num1 = "200";
+
+// let name1 = "Shashank";
+
+// let name2 = 'name1';
+
+// document.write(name1);
+
+// let statement1 = "manish's father name is Manoj ";
+
+// document.write(statement1);
+
+// function greet(msg) {
+//     document.write(msg);
+// }
+
+// greet(`Hey ${prompt("Enter your name")} Good Morning!`);
+
+
+// let username = prompt("Enter your name: ");
+
+// let msg = `Hey ${username} Good Morning!`;
+
+// document.write(msg);
+
+// let username = "OfficialBABA";
+
+// document.write(username.length);
+
+// document.write(username.toLowerCase());
+
+// let email2 = 'purnanandupadhyau@gmail.com';
+// //            0123456789.........
+// let email1 = 'example@gmail.com';
+
+//                ...-3-2-1
+// let username = "Purnanand upadhyay";
+// indexing =>   0123456789.....
+
+// document.write(email1.slice(-4));
+
+// slicing
+// document.write(username[0]);
+// document.write(username.charAt(10))
+// console.log(username[11]);
+// document.write(email1.slice(0, 6));
+
+// console.log(email2.split("@"))
